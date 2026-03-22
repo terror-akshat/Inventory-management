8e63d8e6d88b4a051954c8d4bf0390da56d16409
terror-akshat <www.akshatagarwal123klj@gmail.com>
2026-03-22 19:19:38 +0530

    updating url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://inventory-management-yymx.onrender.com",
+  baseURL: "https://inventory-management-yymx.onrender.com/api",
   timeout: 15000,
 });
 
